613ab35804ceb1fbbe172b0de8a097e6c5b619d3
Jeremy Swafford <jeremy.swafford@willowtreeapps.com>
2022-04-01 10:33:28 -0400

    prefer sprite-based address rendering engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,214 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-debugger */
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import BaseShape, { Orientation } from "./BaseShape";
+import { Color, PieceSprite, Spot, SpriteEntry } from "./types";
 import useKeyPress from "./useKeyPress";
-
-interface Shape {
-  column: Column;
-  row: number;
-  orientation: Orientation;
-  color: string;
-  sprite: number[][];
-}
-
-enum Column {
-  a,
-  b,
-  c,
-  d,
-  e,
-  f,
-  g,
-  h,
-  i,
-  j,
-}
 
 function App() {
   const boardWidth = 10;
   const boardHeight = 12;
-  const empty = "bg-slate-400";
-  const emptyBoard: string[][] = [];
+  const empty: Spot = { color: "bg-slate-400", fixed: false };
+  const emptyBoard: Spot[][] = [];
   for (let y = 0; y < boardHeight; y++) {
-    const tempRow: string[] = [];
+    const tempRow: Spot[] = [];
     for (let x = 0; x < boardWidth; x++) {
       tempRow.push(empty);
     }
     emptyBoard.push(tempRow);
   }
 
-  const pieces = {
+  const pieceSprites: PieceSprite = {
     i: {
+      name: "i",
       color: "bg-blue-400",
-      sprite: [[1], [1], [1], [1]],
+      sprite: [
+        [{ row: 0, col: 4 }],
+        [{ row: 1, col: 4 }],
+        [{ row: 2, col: 4 }],
+        [{ row: 3, col: 4 }],
+      ],
+      height: 4,
+      width: 1,
+      row: 0,
+      column: 4,
     },
     j: {
+      name: "j",
       color: "bg-red-400",
       sprite: [
-        [1, 0, 0],
-        [1, 1, 1],
+        [{ row: 0, col: 4 }],
+        [
+          { row: 1, col: 4 },
+          { row: 1, col: 5 },
+          { row: 1, col: 6 },
+        ],
       ],
+      height: 2,
+      width: 3,
+      row: 0,
+      column: 4,
     },
     l: {
+      name: "l",
       color: "bg-orange-400",
       sprite: [
-        [0, 0, 1],
-        [1, 1, 1],
+        [{ row: 0, col: 6 }],
+        [
+          { row: 1, col: 4 },
+          { row: 1, col: 5 },
+          { row: 1, col: 6 },
+        ],
       ],
+      height: 2,
+      width: 3,
+      row: 0,
+      column: 4,
     },
     o: {
+      name: "o",
       color: "bg-yellow-400",
       sprite: [
-        [1, 1],
-        [1, 1],
+        [
+          { row: 0, col: 4 },
+          { row: 0, col: 5 },
+        ],
+        [
+          { row: 1, col: 4 },
+          { row: 1, col: 5 },
+        ],
       ],
+      height: 2,
+      width: 2,
+      row: 0,
+      column: 4,
     },
     s: {
+      name: "s",
       color: "bg-green-400",
       sprite: [
-        [0, 1, 1],
-        [1, 1, 0],
+        [
+          { row: 0, col: 5 },
+          { row: 0, col: 6 },
+        ],
+        [
+          { row: 1, col: 4 },
+          { row: 1, col: 5 },
+        ],
       ],
+      height: 2,
+      width: 3,
+      row: 0,
+      column: 4,
     },
     t: {
+      name: "t",
       color: "bg-blue-400",
       sprite: [
-        [0, 1, 0],
-        [1, 1, 1],
+        [{ row: 0, col: 5 }],
+        [
+          { row: 1, col: 4 },
+          { row: 1, col: 5 },
+          { row: 1, col: 6 },
+        ],
       ],
+      height: 2,
+      width: 3,
+      row: 0,
+      column: 4,
     },
     z: {
+      name: "z",
       color: "bg-purple-400",
       sprite: [
-        [1, 1, 0],
-        [0, 1, 1],
+        [
+          { row: 0, col: 4 },
+          { row: 0, col: 5 },
+        ],
+        [
+          { row: 1, col: 5 },
+          { row: 1, col: 6 },
+        ],
       ],
+      height: 2,
+      width: 3,
+      row: 0,
+      column: 4,
     },
   };
 
-  const initalPiece: Shape = {
-    row: 0,
-    column: Column.e,
-    orientation: Orientation.Up,
-    color: pieces["i"].color,
-    sprite: pieces["i"].sprite,
-  };
-
-  const [board, setBoard] = useState<string[][]>(emptyBoard);
-  const [piece, setPiece] = useState<Shape>(initalPiece);
+  const [board, setBoard] = useState<Spot[][]>(emptyBoard);
+  const [sprite, setSprite] = useState<SpriteEntry>(pieceSprites.i);
   const [time, setTime] = useState(0);
   const [isPieceSet, setIsPieceSet] = useState(false);
 
   const leftPressed = useKeyPress("ArrowLeft");
   const rightPressed = useKeyPress("ArrowRight");
 
-  useEffect(() => {
+  const colorTheSpots = (
+    theSprite: SpriteEntry,
+    color: Color,
+    fixed: boolean
+  ) => {
     const tempBoard = [...board];
-    piece.sprite.map((myRow, rowIndex) => {
-      myRow.map((spot, colIndex) => {
-        const color = spot === 1 ? piece.color : empty;
-        const myRow = (piece.row + rowIndex) as Column;
-        if (myRow < tempBoard.length) {
-          tempBoard[myRow].splice(piece.column + colIndex, 1, color);
-        }
+    theSprite.sprite.forEach((spriteRow) => {
+      spriteRow.forEach((spriteSpot) => {
+        const isFixed = tempBoard[spriteSpot.row][spriteSpot.col].fixed;
+        !isFixed &&
+          tempBoard[spriteSpot.row].splice(spriteSpot.col, 1, {
+            color: color,
+            fixed,
+          });
       });
     });
     setBoard(tempBoard);
-  }, [piece]);
+  };
+
+  const setTheSprite = (newSprite: SpriteEntry, fixed?: boolean) => {
+    setSprite((oldSprite) => {
+      colorTheSpots(oldSprite, "bg-slate-400", false);
+      colorTheSpots(newSprite, newSprite.color, !!fixed);
+      return newSprite;
+    });
+  };
+
+  useEffect(() => {
+    const tempBoard = [...board];
+    sprite.sprite.forEach((spriteRow, rowIndex) => {
+      spriteRow.forEach((spriteSpot) => {
+        if (rowIndex === 0) {
+          tempBoard[spriteSpot.row - 1]?.splice(spriteSpot.col, 1, {
+            color: "bg-slate-400",
+            fixed: false,
+          });
+        }
+        tempBoard[spriteSpot.row].splice(spriteSpot.col, 1, {
+          color: sprite.color,
+          fixed: false,
+        });
+      });
+    });
+
+    setBoard(tempBoard);
+  }, [sprite]);
 
   useEffect(() => {
     // move piece down G R A V I T Y
-    if (piece.row + piece.sprite.length < boardHeight) {
-      setBoard(
-        board.map((row, index) =>
-          index === piece.row ? new Array(boardWidth).fill(empty) : row
-        )
-      );
-      setPiece({ ...piece, row: piece.row + 1 });
+    if (sprite.row + sprite.height < boardHeight) {
+      setTheSprite({
+        ...sprite,
+        row: sprite.row + 1,
+        sprite: sprite.sprite.map((srow) =>
+          srow.map((spot) => {
+            return {
+              ...spot,
+              row: spot.row + 1,
+            };
+          })
+        ),
+      });
     } else {
+      setTheSprite(sprite, true);
       setIsPieceSet(true);
     }
     setTimeout(() => {
@@ -138,79 +220,65 @@ function App() {
 
   useEffect(() => {
     if (leftPressed && !isPieceSet) {
-      setPiece({ ...piece, column: piece.column - 1 });
-      shiftLeft();
+      setTheSprite({
+        ...sprite,
+        column: sprite.column - 1,
+        sprite: sprite.sprite.map((srow) =>
+          srow.map((spot) => {
+            return {
+              ...spot,
+              col: spot.col - 1,
+            };
+          })
+        ),
+      });
     }
     if (rightPressed && !isPieceSet) {
-      setPiece({ ...piece, column: piece.column + 1 });
-      shiftRight();
+      setTheSprite({
+        ...sprite,
+        column: sprite.column + 1,
+        sprite: sprite.sprite.map((srow) =>
+          srow.map((spot) => {
+            return {
+              ...spot,
+              col: spot.col + 1,
+            };
+          })
+        ),
+      });
     }
   }, [leftPressed, rightPressed]);
 
   useEffect(() => {
-    let nextPiece = pieces["l"];
-    switch (Math.floor(Math.random() * 6)) {
-      case 0:
-        nextPiece = pieces["i"];
+    let nextPiece = pieceSprites["l"];
+    // switch (Math.floor(Math.random() * 6)) {
+    switch (sprite.name) {
+      case "i":
+        nextPiece = pieceSprites["j"];
         break;
-      case 1:
-        nextPiece = pieces["j"];
+      case "j":
+        nextPiece = pieceSprites["l"];
         break;
-      case 2:
-        nextPiece = pieces["l"];
+      case "l":
+        nextPiece = pieceSprites["o"];
         break;
-      case 3:
-        nextPiece = pieces["o"];
+      case "o":
+        nextPiece = pieceSprites["s"];
         break;
-      case 4:
-        nextPiece = pieces["s"];
+      case "s":
+        nextPiece = pieceSprites["t"];
         break;
-      case 5:
-        nextPiece = pieces["t"];
+      case "t":
+        nextPiece = pieceSprites["z"];
         break;
-      case 6:
-        nextPiece = pieces["z"];
-        break;
+      case "z":
       default:
+        nextPiece = pieceSprites["i"];
         break;
     }
-    setPiece({
-      row: 0,
-      column: Column.e,
-      color: nextPiece.color,
-      sprite: nextPiece.sprite,
-      orientation: Orientation.Up,
-    });
+    setTheSprite(nextPiece);
     setIsPieceSet(false);
   }, [isPieceSet]);
-
-  const shiftLeft = () => {
-    const b = board.map((row) => {
-      return row.reduce((cumuRow: string[], current, currentIndex, self) => {
-        if (currentIndex + 1 < self.length) {
-          cumuRow.push(self[currentIndex + 1]);
-        } else {
-          cumuRow.push(empty);
-        }
-        return cumuRow;
-      }, []);
-    });
-    setBoard(b);
-  };
-
-  const shiftRight = () => {
-    const b = board.map((row) => {
-      return row.reduce((cumuRow: string[], current, currentIndex, self) => {
-        if (currentIndex - 1 >= 0) {
-          cumuRow.push(self[currentIndex - 1]);
-        } else {
-          cumuRow.push(empty);
-        }
-        return cumuRow;
-      }, []);
-    });
-    setBoard(b);
-  };
 
   return (
     <div className="">
@@ -228,7 +296,7 @@ function App() {
 export default App;
 
 interface RowProps {
-  spots: string[];
+  spots: Spot[];
 }
 export const Row = ({ spots }: RowProps) => {
   return (
@@ -238,7 +306,7 @@ export const Row = ({ spots }: RowProps) => {
           return (
             <div
               key={`spot-${index}`}
-              className={`${spot} w-16 h-16 m-auto border`}
+              className={`${spot.color} w-16 h-16 m-auto border`}
             ></div>
           );
         })}
---
+++ src/types.ts
@@ -0,0 +1,73 @@
+export type Color =
+  | "bg-slate-400"
+  | "bg-slate-200"
+  | "bg-blue-400"
+  | "bg-red-400"
+  | "bg-orange-400"
+  | "bg-yellow-400"
+  | "bg-green-400"
+  | "bg-pink-400"
+  | "bg-purple-400";
+
+export interface Shape {
+  column: Column;
+  row: number;
+  width: number;
+  height: number;
+  orientation: Orientation;
+  color: Color;
+  sprite: number[][];
+}
+
+export interface ShapeMap {
+  [key: string]: Partial<Shape>;
+}
+
+export interface Spot {
+  row?: number;
+  col?: Column;
+  value?: number;
+  color: Color;
+  fixed: boolean;
+}
+
+export interface SpriteSpot {
+  row: number;
+  col: Column;
+}
+
+export type Sprite = SpriteSpot[][];
+
+export interface SpriteEntry {
+  name: string;
+  color: Color;
+  sprite: Sprite;
+  row: number;
+  column: number;
+  height: number;
+  width: number;
+}
+
+export interface PieceSprite {
+  [key: string]: SpriteEntry;
+}
+
+export enum Column {
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+}
+
+export enum Orientation {
+  Up,
+  Down,
+  Left,
+  Right,
+}
