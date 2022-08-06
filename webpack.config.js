cbad031644d5685d61d974068b3155c7252b92c0
Jeremy Swafford <jeremy.swafford@willowtreeapps.com>
2022-08-06 16:37:30 -0400

    STOP YELLING

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
