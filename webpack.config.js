6b46fad315f67e01fe9ca01d1da471e58beca763
Jeremy Swafford <jeremy.swafford@willowtreeapps.com>
2022-08-06 16:26:20 -0400

    adds webpack config

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:8081/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8081,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.m?js/,
+      //   type: "javascript/auto",
+      //   resolve: {
+      //     fullySpecified: false,
+      //   },
+      // },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+      }
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "tetris",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        './App': './src/App'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: './index.html',
+      favicon: "./public/favicon.ico",
+      manifest: "./public/manifest.ico",
+    }),
+  ],
+};
