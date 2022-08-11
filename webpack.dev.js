ad560c56c8968c1b1d3a5a94d4780075b3ee5055
Jeremy Swafford <jeremy.swafford@willowtreeapps.com>
2022-08-11 15:51:32 -0400

    break webpack config out into separate environments

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,5 +4,9 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
 });
